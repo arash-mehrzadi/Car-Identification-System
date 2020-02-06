cd30523a19d645884b46b3827bbdbbf4aa3d653e
arash mehrzadi <48579846+arash-mehrzadi@users.noreply.github.com>
2020-02-06 04:35:00 -0800

    Add files via upload

---
+++ Support Page/frmcalculate.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Accounting_App
+{
+    public partial class frmcalculate : Form
+    {
+        Form1 frmmain;
+        public frmcalculate(Form1 frm2)
+        {
+            frmmain = frm2;
+            InitializeComponent();
+        }
+
+        private void btnexit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
