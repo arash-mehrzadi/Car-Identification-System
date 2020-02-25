b036ff5f3d25d0fdec6ed08d1a31dc3af4f5dea1
arash mehrzadi <48579846+arash-mehrzadi@users.noreply.github.com>
2020-02-24 22:48:21 -0800

    Add files via upload

---
+++ password setting/frmsetting.Designer.cs
@@ -0,0 +1,237 @@
+﻿namespace Accounting_App
+{
+    partial class frmsetting
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmsetting));
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.txtnpass = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnpassr = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnadd = new ns1.BunifuThinButton2();
+            this.btnexit = new ns1.BunifuThinButton2();
+            this.lblaac = new System.Windows.Forms.Label();
+            this.lblwrongpassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.AliceBlue;
+            this.label4.Location = new System.Drawing.Point(422, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(163, 29);
+            this.label4.TabIndex = 53;
+            this.label4.Text = "تغییر رمز دسترسی";
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(190, 68);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.Size = new System.Drawing.Size(253, 20);
+            this.txtpass.TabIndex = 0;
+            // 
+            // txtnpass
+            // 
+            this.txtnpass.Location = new System.Drawing.Point(190, 106);
+            this.txtnpass.Name = "txtnpass";
+            this.txtnpass.Size = new System.Drawing.Size(253, 20);
+            this.txtnpass.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label1.Location = new System.Drawing.Point(497, 59);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(86, 29);
+            this.label1.TabIndex = 56;
+            this.label1.Text = "رمز فعلی";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label3.Location = new System.Drawing.Point(497, 97);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(87, 29);
+            this.label3.TabIndex = 55;
+            this.label3.Text = "رمز جدید";
+            // 
+            // txtnpassr
+            // 
+            this.txtnpassr.Location = new System.Drawing.Point(190, 145);
+            this.txtnpassr.Name = "txtnpassr";
+            this.txtnpassr.Size = new System.Drawing.Size(253, 20);
+            this.txtnpassr.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.SeaGreen;
+            this.label2.Location = new System.Drawing.Point(448, 136);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(137, 29);
+            this.label2.TabIndex = 59;
+            this.label2.Text = "تکرار رمز جدید";
+            // 
+            // btnadd
+            // 
+            this.btnadd.ActiveBorderThickness = 1;
+            this.btnadd.ActiveCornerRadius = 20;
+            this.btnadd.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnadd.ActiveForecolor = System.Drawing.Color.White;
+            this.btnadd.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnadd.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(99)))));
+            this.btnadd.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnadd.BackgroundImage")));
+            this.btnadd.ButtonText = "ثبت";
+            this.btnadd.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnadd.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnadd.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnadd.IdleBorderThickness = 1;
+            this.btnadd.IdleCornerRadius = 20;
+            this.btnadd.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(51)))), ((int)(((byte)(70)))));
+            this.btnadd.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btnadd.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btnadd.Location = new System.Drawing.Point(41, 123);
+            this.btnadd.Margin = new System.Windows.Forms.Padding(5);
+            this.btnadd.Name = "btnadd";
+            this.btnadd.Size = new System.Drawing.Size(86, 42);
+            this.btnadd.TabIndex = 3;
+            this.btnadd.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnadd.Click += new System.EventHandler(this.btnadd_Click);
+            // 
+            // btnexit
+            // 
+            this.btnexit.ActiveBorderThickness = 1;
+            this.btnexit.ActiveCornerRadius = 20;
+            this.btnexit.ActiveFillColor = System.Drawing.Color.SeaGreen;
+            this.btnexit.ActiveForecolor = System.Drawing.Color.White;
+            this.btnexit.ActiveLineColor = System.Drawing.Color.SeaGreen;
+            this.btnexit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(99)))));
+            this.btnexit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnexit.BackgroundImage")));
+            this.btnexit.ButtonText = "خروج";
+            this.btnexit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnexit.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnexit.ForeColor = System.Drawing.Color.SeaGreen;
+            this.btnexit.IdleBorderThickness = 1;
+            this.btnexit.IdleCornerRadius = 20;
+            this.btnexit.IdleFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(10)))), ((int)(((byte)(51)))), ((int)(((byte)(70)))));
+            this.btnexit.IdleForecolor = System.Drawing.Color.SeaGreen;
+            this.btnexit.IdleLineColor = System.Drawing.Color.SeaGreen;
+            this.btnexit.Location = new System.Drawing.Point(41, 171);
+            this.btnexit.Margin = new System.Windows.Forms.Padding(5);
+            this.btnexit.Name = "btnexit";
+            this.btnexit.Size = new System.Drawing.Size(86, 42);
+            this.btnexit.TabIndex = 4;
+            this.btnexit.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnexit.Click += new System.EventHandler(this.btnexit_Click);
+            // 
+            // lblaac
+            // 
+            this.lblaac.AutoSize = true;
+            this.lblaac.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblaac.ForeColor = System.Drawing.Color.SeaGreen;
+            this.lblaac.Location = new System.Drawing.Point(56, 59);
+            this.lblaac.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblaac.Name = "lblaac";
+            this.lblaac.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblaac.Size = new System.Drawing.Size(81, 29);
+            this.lblaac.TabIndex = 85;
+            this.lblaac.Text = "ثبت شد !";
+            // 
+            // lblwrongpassword
+            // 
+            this.lblwrongpassword.AutoSize = true;
+            this.lblwrongpassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblwrongpassword.ForeColor = System.Drawing.Color.SeaGreen;
+            this.lblwrongpassword.Location = new System.Drawing.Point(11, 9);
+            this.lblwrongpassword.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblwrongpassword.Name = "lblwrongpassword";
+            this.lblwrongpassword.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lblwrongpassword.Size = new System.Drawing.Size(267, 29);
+            this.lblwrongpassword.TabIndex = 86;
+            this.lblwrongpassword.Text = "رمز فعلی یا  تکرار رمز نادرست";
+            // 
+            // frmsetting
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(51)))), ((int)(((byte)(99)))));
+            this.ClientSize = new System.Drawing.Size(596, 227);
+            this.Controls.Add(this.lblwrongpassword);
+            this.Controls.Add(this.lblaac);
+            this.Controls.Add(this.btnexit);
+            this.Controls.Add(this.btnadd);
+            this.Controls.Add(this.txtnpassr);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtnpass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "frmsetting";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "frmsetting";
+            this.Load += new System.EventHandler(this.frmsetting_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.TextBox txtnpass;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnpassr;
+        private System.Windows.Forms.Label label2;
+        private ns1.BunifuThinButton2 btnadd;
+        private ns1.BunifuThinButton2 btnexit;
+        private System.Windows.Forms.Label lblaac;
+        private System.Windows.Forms.Label lblwrongpassword;
+    }
+}
